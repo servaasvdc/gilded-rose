7b7dfd72ac8e92314867d22860b3cce5dae7d600
Servaas Vandecappelle <4853491+servaasvdc@users.noreply.github.com>
2025-09-23 20:58:16 +0200

    fix Foo test

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -12,6 +12,6 @@ public class GildedRoseTest
         IList<Item> items = new List<Item> { new() { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.Equal("fixme", items[0].Name);
+        Assert.Equal("foo", items[0].Name);
     }
 }

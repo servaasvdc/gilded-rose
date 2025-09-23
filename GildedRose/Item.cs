4f050b4a1912747201853ab8a0110aa99362b9ac
Servaas Vandecappelle <4853491+servaasvdc@users.noreply.github.com>
2025-09-23 20:57:35 +0200

    add comments to Item

--- GildedRose/Item.cs
+++ GildedRose/Item.cs
@@ -3,6 +3,17 @@
 public class Item
 {
     public string Name { get; set; }
+    
+    /// <summary>
+    /// Denotes the number of days we have to sell the item. At the end of each day, this value is decremented by 1.
+    /// </summary>
     public int SellIn { get; set; }
+    
+    /// <summary>
+    /// Denotes how valuable the item is.
+    /// </summary>
+    /// <remarks>
+    /// Cannot be negative. There is also a maximum, depending on the exact item.
+    /// </remarks>
     public int Quality { get; set; }
 }

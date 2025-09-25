fd82b4a9499f339ddc983c6b47e745cd0e7d0c27
Servaas Vandecappelle <4853491+servaasvdc@users.noreply.github.com>
2025-09-25 11:26:39 +0200

    early exit when item is Sulfuras

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -6,82 +6,82 @@ public class GildedRose(IList<Item> items)
 {
     public void UpdateQuality()
     {
-        for (var i = 0; i < items.Count; i++)
+        foreach (var item in items)
         {
-            if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name == "Sulfuras, Hand of Ragnaros")
             {
-                if (items[i].Quality > 0)
+                // Rule: "Sulfuras", being a legendary item, never has to be sold or decreases in Quality.
+                continue;
+            }
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality > 0)
                 {
-                    if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+
+                    item.Quality = item.Quality - 1;
+                    
+                    if (item.Name == "Conjured Mana Cake")
                     {
-                        items[i].Quality = items[i].Quality - 1;
-                    }
-                    if (items[i].Name == "Conjured Mana Cake")
-                    {
-                        items[i].Quality = items[i].Quality - 1;
+                        item.Quality = item.Quality - 1;
                     }   
                 }
             }
             else
             {
-                if (items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    items[i].Quality = items[i].Quality + 1;
+                    item.Quality = item.Quality + 1;
 
-                    if (items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (items[i].SellIn < 11)
+                        if (item.SellIn < 11)
                         {
-                            if (items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                items[i].Quality = items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
 
-                        if (items[i].SellIn < 6)
+                        if (item.SellIn < 6)
                         {
-                            if (items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                items[i].Quality = items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
                 }
             }
+            
+            item.SellIn = item.SellIn - 1;
 
-            if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+            if (item.SellIn < 0)
             {
-                items[i].SellIn = items[i].SellIn - 1;
-            }
-
-            if (items[i].SellIn < 0)
-            {
-                if (items[i].Name != "Aged Brie")
+                if (item.Name != "Aged Brie")
                 {
-                    if (items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (items[i].Quality > 0)
+                        if (item.Quality > 0)
                         {
-                            if (items[i].Name != "Sulfuras, Hand of Ragnaros")
+
+                            item.Quality = item.Quality - 1;
+                            
+                            if (item.Name == "Conjured Mana Cake")
                             {
-                                items[i].Quality = items[i].Quality - 1;
-                            }
-                            if (items[i].Name == "Conjured Mana Cake")
-                            {
-                                items[i].Quality = items[i].Quality - 1;
+                                item.Quality = item.Quality - 1;
                             }
                         }
                     }
                     else
                     {
-                        items[i].Quality = items[i].Quality - items[i].Quality;
+                        item.Quality = item.Quality - item.Quality;
                     }
                 }
                 else
                 {
-                    if (items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        items[i].Quality = items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
